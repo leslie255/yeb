ad31b952553c63a142c32e28799561c23642be2c
Leslie Kerman <kerbal255@outlook.com>
2024-04-26 22:03:41 +0100

    Pass command line arguments into `build.c`

--- build.c
+++ build.c
@@ -4,19 +4,22 @@ void cc(Cmd *cmd) { CMD_APPEND(cmd, "clang"); }
 
 void cflags(Cmd *cmd) { CMD_APPEND(cmd, "-Wall", "-Wextra"); }
 
-Cmd build_test() {
+Cmd build_test_main() {
   Cmd cmd = {0};
   cc(&cmd);
   cflags(&cmd);
-  CMD_APPEND(&cmd, "main.c", "-o", "test");
+  CMD_APPEND(&cmd, "test_main.c", "-o", "test");
   return cmd;
 }
 
-int main() {
+int main(int argc, char **argv) {
   yeb_bootstrap();
+  for (int i = 1; i < argc; ++i) {
+    printf("%s\n", argv[i]);
+  }
   Cmd cmd = {0};
   cc(&cmd);
   cflags(&cmd);
-  execute(build_test());
+  execute(build_test_main());
   return 0;
 }
--- yeb.h
+++ yeb.h
@@ -31,11 +31,11 @@ void yeb_bootstrap();
 #define da_append(DA, ITEMS, N)                                                \
   do {                                                                         \
     __auto_type DA_ = (DA);                                                    \
-    size_t n = (N);                                                            \
-    size_t i = DA_->da_len;                                                    \
-    DA_->da_len += n;                                                          \
+    size_t N_ = (N);                                                           \
+    size_t I_ = DA_->da_len;                                                   \
+    DA_->da_len += N_;                                                         \
     if (DA_->da_items == NULL) {                                               \
-      DA_->da_cap = DA_INIT_CAP > n ? DA_INIT_CAP : n;                         \
+      DA_->da_cap = DA_INIT_CAP > N_ ? DA_INIT_CAP : N_;                       \
       DA_->da_items = malloc(sizeof(DA_->da_items[0]) * DA_->da_cap);          \
       assert(DA_->da_items != NULL);                                           \
     } else if (DA_->da_len > DA_->da_cap) {                                    \
@@ -45,7 +45,7 @@ void yeb_bootstrap();
           realloc(DA_->da_items, sizeof(DA_->da_items[0]) * DA_->da_cap);      \
       assert(DA_->da_items != NULL);                                           \
     }                                                                          \
-    memcpy(&DA_->da_items[i], (ITEMS), sizeof(DA_->da_items[0]) * n);          \
+    memcpy(&DA_->da_items[I_], (ITEMS), sizeof(DA_->da_items[0]) * N_);        \
   } while (0)
 
 #define da_append_multiple(DA, ...)                                            \
@@ -113,82 +113,9 @@ typedef uint8_t LogLevel;
 #define LOG_LEVEL LOGLEVEL_INFO
 #endif
 
-void lprintf(LogLevel level, const char *restrict fmt, ...);
-
 #ifndef YEB_NO_IMPL
 
-// #undef YEB_INTERNAL
-#ifndef YEB_INTERNAL
-
-#define YEB_ERROR_NOT_BOOTSTRAPPED()                                           \
-  (printf("You need to call `yeb_bootstrap()` before doing anything in "       \
-          "`build.c`\n"),                                                      \
-   exit(1))
-
-void execute(Cmd cmd) { YEB_ERROR_NOT_BOOTSTRAPPED(); }
-
-void yeb_bootstrap() {
-  FILE *yeb_is_bootstrapped_txt = fopen("yeb/yeb_is_bootstrapped.txt", "r");
-  if (yeb_is_bootstrapped_txt != NULL) {
-    fclose(yeb_is_bootstrapped_txt);
-    printf("Yeb is bootstrapped, now do `./yeb/yeb` to build the project.");
-    return;
-  }
-
-  int exit_code;
-  const char *cmd;
-
-#define yeb_bootstrap_execute(S)                                               \
-  printf("$ %s\n", S);                                                         \
-  cmd = S;                                                                     \
-  exit_code = system(S);                                                       \
-  if (exit_code != 0)                                                          \
-    goto error;
-
-  yeb_bootstrap_execute("mkdir -p yeb/");
-  yeb_bootstrap_execute("cp yeb.h yeb/yeb.h");
-  FILE *yeb_c = fopen("yeb/yeb.c", "w");
-  assert(yeb_c != NULL);
-  fprintf(yeb_c, //
-          "#define YEB_INTERNAL\n"
-          "#include \"yeb.h\"\n");
-  fclose(yeb_c);
-  yeb_bootstrap_execute("clang -c yeb/yeb.c -o yeb/yeb.o");
-  FILE *run_c = fopen("yeb/run.c", "w");
-  assert(run_c != NULL);
-  fprintf(
-      run_c,
-      "#include <stdlib.h>\n"
-      "int main() {\n"
-      "  int exit_code;\n"
-      "  exit_code = system(\"clang -c -DYEB_NO_IMPL build.c -o "
-      "yeb/build.o\");\n"
-      "  if (exit_code != 0) exit(exit_code);\n"
-      "  exit_code = system(\"clang yeb/yeb.o yeb/build.o -o yeb/a.out\");\n"
-      "  if (exit_code != 0) exit(exit_code);\n"
-      "  exit_code = system(\"./yeb/a.out\");\n"
-      "  exit(exit_code);\n"
-      "}");
-  fclose(run_c);
-  yeb_bootstrap_execute("clang yeb/run.c -o yeb/yeb");
-  yeb_bootstrap_execute("touch yeb/yeb_is_bootstrapped.txt");
-  yeb_is_bootstrapped_txt = fopen("yeb/yeb_is_bootstrapped.txt", "w");
-  fprintf(yeb_is_bootstrapped_txt,
-          "This file is here so yeb knows it's already been bootstrapped.\n"
-          "If you're experiencing problems with yeb, try removing this file "
-          "(or the yeb/ directory) and bootstrap again.\n");
-  fclose(yeb_is_bootstrapped_txt);
-  printf("Yeb is bootstrapped, now do `./yeb/yeb` to build the project.\n");
-  exit(0);
-
-error:
-  if (exit_code != 0) {
-    printf("command `%s` exited with non-zero exit code (%d)", cmd, exit_code);
-    exit(1);
-  }
-}
-
-#else
+#ifdef YEB_INTERNAL
 
 void yeb_bootstrap(){};
 
@@ -212,9 +139,96 @@ static inline const char *concat_strings_with_space(ConstStrings strings) {
 void execute(Cmd cmd) {
   const char *s = concat_strings_with_space(cmd.args);
   printf("$ %s\n", s);
-  system(s);
+  int exit_code = system(s);
+  if (exit_code != 0) {
+    printf("YEB: command finished with non-zero exit code (%d)\n", exit_code);
+    exit(exit_code);
+  }
 }
 
-#endif // YEB_INTERNAL
+#else
+#ifdef YEB_MAIN
+
+int main(int argc, char **argv) {
+  int exit_code;
+  exit_code = system("clang -c -DYEB_NO_IMPL build.c -o yeb/build.o");
+  if (exit_code != 0)
+    return exit_code;
+  exit_code = system("clang yeb/yeb.o yeb/build.o -o yeb/a.out");
+  if (exit_code != 0)
+    return exit_code;
+  DynString cmd = {0};
+  da_append(&cmd, "./yeb/a.out", sizeof("./yeb/a.out") - 1);
+  for (int i = 1; i < argc; ++i) {
+    da_push(&cmd, ' ');
+    size_t len = strlen(argv[i]);
+    da_append(&cmd, argv[i], len);
+  }
+  da_push(&cmd, '\0');
+  printf("[%s:%d] cmd.da_items = %s\n", __FILE__, __LINE__, cmd.da_items);
+  return system(cmd.da_items);
+}
+
+#else
+
+#define YEB_ERROR_NOT_BOOTSTRAPPED()                                           \
+  (printf("You need to call `yeb_bootstrap()` before doing anything in "       \
+          "`build.c`\n"),                                                      \
+   exit(1))
+
+void execute(Cmd cmd) { YEB_ERROR_NOT_BOOTSTRAPPED(); }
+
+void yeb_bootstrap() {
+  FILE *yeb_is_bootstrapped_txt = fopen("yeb/yeb_is_bootstrapped.txt", "r");
+  if (yeb_is_bootstrapped_txt != NULL) {
+    fclose(yeb_is_bootstrapped_txt);
+    printf(
+        "Yeb was already bootstrapped, use `./yeb/yeb` to build the project.\n"
+        "If you want to boostrap again because it's not working correctly, "
+        "delete `yeb/` and boostrap again.\n");
+    exit(0);
+  }
+
+  int exit_code;
+
+#define yeb_bootstrap_execute(S)                                               \
+  printf("$ %s\n", S);                                                         \
+  exit_code = system(S);                                                       \
+  if (exit_code != 0)                                                          \
+    if (exit_code != 0) {                                                      \
+      printf("command `%s` exited with non-zero exit code (%d)", S,            \
+             exit_code);                                                       \
+      exit(1);                                                                 \
+    }
+
+  yeb_bootstrap_execute("mkdir -p yeb/");
+  yeb_bootstrap_execute("cp yeb.h yeb/yeb.h");
+  FILE *yeb_c = fopen("yeb/yeb.c", "w");
+  assert(yeb_c != NULL);
+  fprintf(yeb_c, //
+          "#define YEB_INTERNAL\n"
+          "#include \"yeb.h\"\n");
+  fclose(yeb_c);
+  yeb_bootstrap_execute("clang -c yeb/yeb.c -o yeb/yeb.o");
+  FILE *run_c = fopen("yeb/run.c", "w");
+  assert(run_c != NULL);
+  fprintf(run_c, //
+          "#define YEB_MAIN\n"
+          "#include \"yeb.h\"\n");
+  fclose(run_c);
+  yeb_bootstrap_execute("clang yeb/run.c -o yeb/yeb");
+  yeb_bootstrap_execute("touch yeb/yeb_is_bootstrapped.txt");
+  yeb_is_bootstrapped_txt = fopen("yeb/yeb_is_bootstrapped.txt", "w");
+  fprintf(yeb_is_bootstrapped_txt,
+          "This file is here so yeb knows it's already been bootstrapped.\n"
+          "If you're experiencing problems with yeb, try removing this file "
+          "(or the yeb/ directory) and bootstrap again.\n");
+  fclose(yeb_is_bootstrapped_txt);
+  printf("Yeb is bootstrapped, now do `./yeb/yeb` to build the project.\n");
+  exit(0);
+}
+
+#endif
+#endif
 
 #endif // YEB_NO_IMPL
